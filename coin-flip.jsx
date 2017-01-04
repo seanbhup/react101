6140172aa3d2f5fc57c50490c12d46f8b1ac3994
Sean Bhupathi <seanbhupathi@gmail.com>
2017-01-04 09:04:08 -0500

    coin now flips 1 time

--- coin-flip.jsx
+++ coin-flip.jsx
@@ -1,21 +1,41 @@
 var Application = React.createClass({
-	getInitialState: function() {
-		return{
-			image: "http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg"
-		}
-	}
-
 	render: function(){
 		return(
 			<div className="application">
-				<button onClick={flipCoin}>Click to flip</button>
-				<div><img src={this.state.image}></div>
+				<div>
+					<Coin />
+				</div>
 			</div>
 		)
 	}
 });
 
+
+var Coin = React.createClass({
+	getInitialState: function() {
+		return{
+			image: 'http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg'
+		}
+	},
+	flipCoin: function(){
+		this.setState({
+			image: "http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-head.jpg"
+		})
+	},
+	render: function(){
+		return(
+			<div className="flip">
+				<button onClick={this.flipCoin}>Click to Flip</button>
+				<div><img src={this.state.image} /></div>
+			</div>
+		)
+	}
+})
+
+
 ReactDOM.render(
-	<Application imageSrc="http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg" />,
-	document.getElementById("container")
-)
+	<Application imageSrc = 'http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg' />, 
+	document.getElementById('container')
+);
+
+// http://www.marshu.com/articles/images-website/articles/presidents-on-coins/quarter-coin-head.jpg
